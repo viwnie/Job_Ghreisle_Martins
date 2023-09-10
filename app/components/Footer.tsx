4a3934989ca6ec64529f26034c174536ab8ad5be
nanerseasac <renan.casaeses@gmail.com>
2023-09-10 15:54:24 -0300

    Done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
 	return (
-		<footer className="flex w-full bg-blue-500 text-white p-10 justify-between max-sm:flex-col max-sm:gap-10 max-sm:items-center ">
-			<div className="flex flex-col gap-2 ml-20  text-[20px] max-sm:ml-0 max-sm:justify-center bg-red-500">
-				<div className="flex bg-green-500">
+		<footer className="flex w-full bg-black text-white p-10 justify-between max-md:flex-col max-md:gap-10 max-md:items-center ">
+			<div className="flex flex-col gap-2 ml-20  text-[16px] max-md:ml-0 max-md:justify-center">
+				<div>
+					<h1 className="font-alt text-4xl pt-4 pb-4">Ghreisle Cleaning</h1>
+				</div>
+				<div className="flex">
 					<div className="">
 						<span
 							className="material-symbols-outlined text-white"
@@ -69,7 +72,7 @@ const Footer = () => {
 					<p className=" pl-1">Lunes a Viernes de 7:00 a 22:00</p>
 				</div>
 			</div>
-			<div className="flex justify-center">
+			<div className="mr-20 max-md:mr-0">
 				<img src="https://via.placeholder.com/300x285" alt="" />
 			</div>
 		</footer>
--- app/components/Prices.tsx
+++ app/components/Prices.tsx
@@ -1,6 +1,16 @@
 const Prices = () => {
     return (
-      <div>Prices</div>
+      <div className="flex w-full bg-white text-black p-10 justify-between max-md:flex-col max-md:gap-10 max-md:items-center ">
+        <div className="bg-blue-500 w-40 h-40">
+
+        </div>
+        <div className="bg-blue-500 w-40 h-40">
+
+        </div>
+        <div className="bg-blue-500 w-40 h-40">
+
+        </div>
+		</div>
     )
   }
   
