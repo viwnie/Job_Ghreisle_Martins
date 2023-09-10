8448b84666e0eb9ef921c42bca6be58f8360ac62
Vinicio Salazar barbosa <viniciosalazar133@gmail.com>
2023-09-10 13:29:15 -0300

    Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import { Navbar, Hero, About, Categorys, Experience, Works, Prices, Feedbacks, Contact, Footer } from "./components"
 
 export default function Home() {
   return (
-    <div>
-      Hello
+<div className="relative z-0 bg-primary" >
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Categorys />
+      <Experience />
+      <Works />
+      <Prices />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <Footer />
+      </div>
     </div>
   )
 }
