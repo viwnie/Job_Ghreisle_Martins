6770e19d0e1367932250eabae4e4f0efe78cd20e
nanerseasac <renan.casaeses@gmail.com>
2023-09-10 17:35:09 -0300

    mudando a cor da letra do layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,16 @@
 const Hero = () => {
-    return (
-      <div>Hero</div>
-    )
-  }
-  
-  export default Hero
+	return (
+		<div>
+			<h1>Get Started</h1>
+			<p>
+				Transform your house and your trade into a sparkling sanctuary with our
+				exceptional house cleaning services. Book now and watch the magic unfold!
+			</p>
+      <button className="bg-black mt-8 rounded-[10px] h-10 w-[120px] text-white hover:bg-blue-700 transition duration-300">
+            <p>Book now</p>
+          </button>
+		</div>
+	);
+};
+
+export default Hero;
