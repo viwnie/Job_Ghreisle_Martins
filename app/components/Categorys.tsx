30815a184e151869cb17426dd185503a599e288f
Vinicio Salazar barbosa <viniciosalazar133@gmail.com>
2023-09-11 19:01:49 -0300

    Category semi-final

--- app/components/Categorys.tsx
+++ app/components/Categorys.tsx
@@ -1,6 +1,25 @@
 const Categorys = () => {
     return (
-      <div>Categorys</div>
+      <div className="w-full justify-evenly flex items-center  px-[400px] py-20">
+        <div className="bg-[#F2F2F2] w-72 h-96 flex flex-col items-center justify-center rounded-2xl">
+          <div className="bg-white w-[85%] h-[50%] flex items-center justify-center rounded-lg">
+          <svg className="h-24 w-24 text-red-600"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />  <line x1="13" y1="7" x2="13" y2="7.01" />  <line x1="17" y1="7" x2="17" y2="7.01" />  <line x1="17" y1="11" x2="17" y2="11.01" />  <line x1="17" y1="15" x2="17" y2="15.01" /></svg>
+          </div>
+          <span>We provide high-quality residential cleaning services.</span>
+        </div>
+        <div className="bg-[#F2F2F2] w-72 h-96 flex flex-col items-center justify-center rounded-2xl" >
+        <div className="bg-white w-[85%] h-[50%] flex items-center justify-center rounded-lg">
+        <svg className="h-24 w-24 text-red-600"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M12 3l5 5a7 7 0 1 1 -10 0l5 -5" /></svg>
+        </div>
+        <span>Carpet cleaning, upholstery, sofas, armchairs, and other furniture cleaning.</span>
+        </div>
+        <div className="bg-[#F2F2F2] w-72 h-96 flex flex-col items-center justify-center rounded-2xl">
+        <div className="bg-white w-[85%] h-[50%] flex items-center justify-center rounded-lg">
+        <svg className="h-24 w-24 text-red-600"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="3.6" y1="9" x2="20.4" y2="9" />  <line x1="3.6" y1="15" x2="20.4" y2="15" />  <path d="M11.5 3a17 17 0 0 0 0 18" />  <path d="M12.5 3a17 17 0 0 1 0 18" /></svg>
+        </div>
+        <span>Availability and flexibility in your appointments through our website.</span>
+        </div>
+      </div>
     )
   }
   
