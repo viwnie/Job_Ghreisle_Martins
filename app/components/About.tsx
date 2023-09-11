49767c4e391f140a62b4a07be547d53ce0774451
Vinicio Salazar barbosa <viniciosalazar133@gmail.com>
2023-09-11 14:02:51 -0300

    About finalizado

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,9 @@
 const About = () => {
     return (
-      <div>About</div>
+      <div className="flex justify-center bg-[#EEEEEE] h-64 items-center ">
+        <h1 className="text-red-600 font-alt text-4xl max-w-[500px]">Interested in acquiring an amazing cleaning service?</h1>
+        <p className="text-black text-x max-w-[400px]">Exhausted from dwelling in a cluttered residence? Allow our proficient cleaning specialists to introduce neatness and organization throughout your abode. We promise to astonish you with the transformation of your own living quarters!</p>
+      </div>
     )
   }
   
